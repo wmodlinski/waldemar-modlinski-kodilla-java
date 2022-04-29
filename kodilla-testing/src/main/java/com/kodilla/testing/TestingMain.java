5f32bd5435a0d2ac3f384c7b67ac36fc17286d47
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-04-29 14:21:32 +0200

    6.2_Czym_jest_TDD?

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        System.out.println("Start testu nr. 1:");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Start testu nr. 2:");
+
+        Calculator calculator = new Calculator();
+        int addAtoB = calculator.add(9, 7);
+        if (addAtoB == 16) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Start testu nr. 3:");
+
+        int substractAtoB = calculator.substract(10, 8);
+        if (substractAtoB == 2) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int substract(int a, int b){
+        return a - b;
+    }
+}
+
