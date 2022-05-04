b27f72d6e31405cc806420b1197fbbefc8b6bfe9
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-05-04 12:41:46 +0200

    6.2 - Poprawione zadanie

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,8 +1,6 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    int a;
-    int b;
 
     public int add(int a, int b){
         return a + b;
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -0,0 +1,5 @@
+package com.kodilla.testing.forum;
+
+public class ForumTestSuite {
+
+}
