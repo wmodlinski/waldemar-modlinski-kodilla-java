60eb9c9f4c5d930fcab2b797e46c125cafc4d692
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-05-13 15:38:06 +0200

    7.2. Niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -9,6 +10,10 @@ import java.util.Locale;
 public class StreamMain {
 
     public static void main(String[] args) {
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(25);
+
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("Lambda", text -> "ABC " + text + " ABC");
