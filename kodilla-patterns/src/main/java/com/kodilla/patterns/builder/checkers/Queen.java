8cfcb2ccc02e48ac88f8c3a1e5b5824aab86548c
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-07-12 16:49:53 +0200

    15.5 Builder

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/checkers/Queen.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns.builder.checkers;
+
+public final class Queen extends Figure {
+
+    public Queen(String color) {
+        super(color);
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Bułka z sezamem")
+                .burgers(2)
+                .sauce("Barbecue")
+                .ingredient("Cebula")
+                .ingredient("Pikle")
+                .build();
+        System.out.println(bigmac);
+
+        //When
+        int howManybuns = bigmac.getBurgers();
+        //Then
+        Assert.assertEquals(2, howManybuns);
+    }
+}
