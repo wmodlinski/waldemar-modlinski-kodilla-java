6d80c1ad7238d22690873b9f139f465ceabd6ebc
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-07-15 11:31:20 +0200

    16.6 - Aktywni użytkownicy

---
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -0,0 +1,34 @@
+package com.kodilla.jdbc;
+
+import java.sql.*;
+import java.util.Properties;
+
+public enum DbManager {
+
+    INSTANCE;
+
+    private Connection conn;
+
+    DbManager() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "kodilla_user");
+        connectionProps.put("password", "kodilla_Pass123");
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/kodilla_course" +
+                            "?serverTimezone=Europe/Warsaw" +
+                            "&useSSL=False",
+                    connectionProps);
+        } catch (SQLException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static DbManager getInstance() {
+        return INSTANCE;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+}
