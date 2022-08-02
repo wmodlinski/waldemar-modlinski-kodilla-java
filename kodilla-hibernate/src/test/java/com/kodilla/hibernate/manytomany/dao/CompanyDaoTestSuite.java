37894644a332d8b7678052e3e21bc86dec835e2e
Waldemar Modlinski <waldemarmodlinski@gmail.com>
2022-08-02 10:12:30 +0200

    17.4 - Dodany test

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -107,12 +107,71 @@ class CompanyDaoTestSuite {
         List<Employee> retrieveEmployeeByLastName = employeeDao.retrieveEmployeeByLastName("Smith");
         System.out.println(1.5);
         System.out.println(retrieveEmployeeByLastName.get(0).getLastname());
-        List<Company> retrieveCompaniesWithThreeFirstLettersAsGiven
-                = companyDao.retrieveCompaniesWithThreeFirstLettersAsGiven("Sof");
 
         System.out.println(2);
         //Then
         assertNotEquals(0, retrieveEmployeeByLastName.size());
+
+        //CleanUp
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+            employeeDao.deleteById(johnSmithId);
+            employeeDao.deleteById(stephanieClarcksonId);
+            employeeDao.deleteById(lindaKovalskyId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+
+    @Test
+    void TestRetrieveCompaniesWithLettersAsGiven() {
+
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        employeeDao.save(johnSmith);
+        int johnSmithId = johnSmith.getId();
+        employeeDao.save(stephanieClarckson);
+        int stephanieClarcksonId = stephanieClarckson.getId();
+        employeeDao.save(lindaKovalsky);
+        int lindaKovalskyId = lindaKovalsky.getId();
+
+        System.out.println(1);
+
+        //When
+        List<Company> retrieveCompaniesWithThreeFirstLettersAsGiven
+                = companyDao.retrieveCompaniesWithThreeFirstLettersAsGiven("Sof");
+
+        System.out.println(2);
+
+        //Then
         assertNotEquals(0, retrieveCompaniesWithThreeFirstLettersAsGiven.size());
 
         //CleanUp
