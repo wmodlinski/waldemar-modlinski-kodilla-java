fe7ff0e8c02d0271e35fb1d6586b4e7085ab865c
Waldemar Modliński <103046767+wmodlinski@users.noreply.github.com>
2022-11-03 16:00:53 +0100

    24.3 - Programowanie aspektowe w Spring Boot

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/FacadeWatcher.java
@@ -0,0 +1,35 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(com.kodilla.patterns2.aop.calculator.Watcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("\"processOrder()\" was used");
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed " + (end - begin) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+}
