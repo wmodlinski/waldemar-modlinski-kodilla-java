123483c90f0c066c00cd560f53d5a1b15964f807
Waldemar Modliński <103046767+wmodlinski@users.noreply.github.com>
2022-11-09 15:52:35 +0100

    30.2 - Testy funkcji UpdateBestsellerStatus()

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -33,4 +33,28 @@ public class StoredProcTestSuite {
         rs.close();
         statement.close();
     }
+
+    @Test
+    void testUpdateBestsellers() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=0";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        // When
+        String sqlProcedureCall = "CALL UpdateBestsellerStatus()";
+        statement.execute(sqlProcedureCall);
+
+        // Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=0";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(3, howMany);
+        rs.close();
+        statement.close();
+    }
 }
